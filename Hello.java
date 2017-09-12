4d32d0129df15504afa74b201958369b19510dc3
Logan Ladd Bielewicz <lbielewicz@hartal.cs.arizona.edu>
2017-09-11 17:59:21 -0700

    second gold leader changes made

--- Hello.java
+++ Hello.java
@@ -16,8 +16,7 @@ public class Hello {
 
     public static void goldLeader() {
 	
-        // Gold Leader should add a print statement that says "Loosen up!"
-    	
+ 	system.out.println( "Loosen up!" );   	
 
     }
 
