83c82a7982c58816f94d4dcdd9d6ec3c0eb292ff
Logan Ladd Bielewicz <lbielewicz@hartal.cs.arizona.edu>
2017-09-11 17:55:39 -0700

    gold leader changes added

--- Hello.java
+++ Hello.java
@@ -11,14 +11,13 @@ public class Hello {
     public static void callSign() {
         
         system.out.println( "Lock S-foils in attack position." );
-        // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-
+	system.out.println( "Stabilize your rear deflectors... Watch for enemy fighters." );
     }
 
     public static void goldLeader() {
-
+	
         // Gold Leader should add a print statement that says "Loosen up!"
-    
+    	
 
     }
 
